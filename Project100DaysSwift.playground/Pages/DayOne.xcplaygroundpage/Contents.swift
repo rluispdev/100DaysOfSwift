7c0e29f664b8060ebcf9f1675164eaa7e29548e4
Rafael Gonzaga via Xcode <rluisp@icloud.com>
2024-01-22 17:40:01 -0300

    Variables,  String and integers

--- Project100DaysSwift.playground/Pages/DayOne.xcplaygroundpage/Contents.swift
+++ Project100DaysSwift.playground/Pages/DayOne.xcplaygroundpage/Contents.swift
@@ -4,8 +4,12 @@ import Foundation
 
 // Steps: Variables, Strings and integers, Multipline strings, Doubles and Booleans, String interpolation, Constants, Type annotations, Simple Types: Summary
 
+//Variables
+var str = "Hello, playground"
+str = "GoodBye"
 
+//MARK: - String and integers
 
- 
+var age = 38
+var population = 8_000_000
 
- 
