a3bf810b25753a2bb1674a058ce7a5f54c2b8927
Rafael Gonzaga via Xcode <rluisp@icloud.com>
2024-01-22 17:26:45 -0300

    Commit inicial - Dia 1

---
+++ Project100DaysSwift.playground/Pages/DayOne.xcplaygroundpage/Contents.swift
@@ -0,0 +1,11 @@
+//: [Previous](@previous)
+
+import Foundation
+
+// Steps: Variables, Strings and integers, Multipline strings, Doubles and Booleans, String interpolation, Constants, Type annotations, Simple Types: Summary
+
+
+
+ 
+
+ 
