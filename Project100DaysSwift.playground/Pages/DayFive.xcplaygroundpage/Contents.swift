f2d44ef401ec0e7bf945c6decab0b7d775e42770
Rafael Gonzaga via Xcode <rluisp@icloud.com>
2024-01-26 15:18:08 -0300

    Writing Functions, Accepting parameters, Returning values

--- Project100DaysSwift.playground/Pages/DayFive.xcplaygroundpage/Contents.swift
+++ Project100DaysSwift.playground/Pages/DayFive.xcplaygroundpage/Contents.swift
@@ -1,7 +1,39 @@
-//: [Previous](@previous)
+//Day5 - WritingFunctions, Accepting paramenters, Returning values, Paramenter Labels, Omitting paramenter labels, Default paramenters, Variadic functions, Writing thowing functions, Running thowing functions, inout parameters
 
-import Foundation
+//MARK: Writing Functions
 
-var greeting = "Hello, playground"
+func printHelp() {
+    let message = """
+Welcome to MyApp!
 
-//: [Next](@next)
+Run this app inside a directory of images and
+MyApp will resize them all into thumbnails
+"""
+
+    print(message)
+}
+///Call function
+printHelp()
+
+//MARK: - Accepting parameters
+
+func square(number: Int) {
+    print(number * number)
+}
+
+square(number: 8)
+
+//MARK: - Returning values
+///->  return
+func square2(number: Int) -> Int {
+    return number * number
+}
+
+let result = square2(number: 83)
+print(result)
+
+ 
+//MARK: - Parameter labels
+
+
+ 
