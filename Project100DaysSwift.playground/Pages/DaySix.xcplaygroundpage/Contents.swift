1c969ec78760791307f677102431fecddf468be9
Rafael Gonzaga via Xcode <rluisp@icloud.com>
2024-01-27 23:50:35 -0300

    Closures as parameters

--- Project100DaysSwift.playground/Pages/DaySix.xcplaygroundpage/Contents.swift
+++ Project100DaysSwift.playground/Pages/DaySix.xcplaygroundpage/Contents.swift
@@ -30,6 +30,20 @@ print(message)
 
 /// To  closure returns a string rather than printing the message directly, so we need to use -> String before in, then use return just like a normal function:
 
-//MARK: -
+//MARK: - Closures as parameters
+
+let driving2 = {
+    print("I'm driving in my car")
+}
+///If we wanted to pass that closure into a function so it can be run inside that function, we would specify the parameter type as () -> Void.
+func travel2(action: () -> Void) {
+    print("I'm getting ready to go.")
+    action()
+    print("I arrived!")
+}
+
+travel2(action: driving2)
+
+
 //MARK: -
 //MARK: -
