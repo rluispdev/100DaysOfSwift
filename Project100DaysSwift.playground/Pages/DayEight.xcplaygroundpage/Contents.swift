f7979242b3107a5e60203063af8d2871ab740f8c
Rafael Gonzaga via Xcode <rluisp@icloud.com>
2024-01-29 15:42:31 -0300

    Properties and methods of strings

--- Project100DaysSwift.playground/Pages/DayEight.xcplaygroundpage/Contents.swift
+++ Project100DaysSwift.playground/Pages/DayEight.xcplaygroundpage/Contents.swift
@@ -126,3 +126,13 @@ struct Person {
 var person = Person(name: "Ed")
 person.makeAnonymous()
 
+//MARK: - Properties and methods of strings
+
+let string = "Do or do not, there is no try."
+print(string.count)
+print(string.hasPrefix("Do"))
+print(string.uppercased())
+print(string.sorted())
+
+
+
