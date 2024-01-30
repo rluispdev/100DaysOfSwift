8c7ce486ae5fa708a76ff247914f8cc59409df46
Rafael Gonzaga via Xcode <rluisp@icloud.com>
2024-01-30 15:07:39 -0300

    Referring to the current instance

--- Project100DaysSwift.playground/Pages/DayNine.xcplaygroundpage/Contents.swift
+++ Project100DaysSwift.playground/Pages/DayNine.xcplaygroundpage/Contents.swift
@@ -15,3 +15,42 @@ var user = User()
 user.username = "twostraws"
 
 //MARK: - Referring to the current instance
+
+
+///Each of these structures uses a different approach to name properties and initializer parameters.
+
+//Using self.
+struct Person {
+    var name: String
+
+    init(name: String) {
+        print("\(name) was born!")
+        self.name = name
+    }
+}
+
+//Approach 2
+struct Person2 {
+    var bear: String
+
+    init(name: String) {
+        print("\(name) was born!")
+        bear = name
+    }
+}
+
+//Approach 3
+struct Person3 {
+var name: String
+
+init(bear: String) {
+    print("\(bear) was born!")
+   name = bear
+}
+}
+
+let test = Person(name: "Kevin")
+let test2 = Person2(name: "Pooh")
+let test3 = Person3(bear: "Lulu")
+
+ 
