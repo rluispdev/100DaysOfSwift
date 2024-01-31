7eab367cc6b21672c4ef1e6c663deb370f3a9a3b
Rafael Gonzaga via Xcode <rluisp@icloud.com>
2024-01-31 15:15:05 -0300

    Creating your own classes

--- Project100DaysSwift.playground/Pages/DayTen.xcplaygroundpage/Contents.swift
+++ Project100DaysSwift.playground/Pages/DayTen.xcplaygroundpage/Contents.swift
@@ -1,1 +1,17 @@
-//Day10 
+//Day10 - Creating your own classes, Creating your own classes, Overriding methods, Final classes, Copying objects, Deinitializers, Mutability
+
+//MARK: Creating your own classes
+
+class Dog {
+    var name: String
+    var breed: String
+
+    init(name: String, breed: String) {
+        self.name = name
+        self.breed = breed
+    }
+}
+
+let poppy = Dog(name: "Poppy", breed: "Poodle")
+
+
