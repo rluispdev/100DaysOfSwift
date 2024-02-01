0d14f45b661b44c8772e3a73beef6a51ef10b3de
Rafael Gonzaga via Xcode <rluisp@icloud.com>
2024-02-01 14:46:55 -0300

    Protocols

--- Project100DaysSwift.playground/Pages/DayEleven.xcplaygroundpage/Contents.swift
+++ Project100DaysSwift.playground/Pages/DayEleven.xcplaygroundpage/Contents.swift
@@ -2,3 +2,35 @@
 
 
 //MARK: Protocols
+
+protocol Identifiable {
+    var id: String { get set }
+}
+
+struct User: Identifiable {
+    var id: String
+    
+
+}
+
+func displayID(thing: Identifiable) {
+    print("My ID is \(thing.id)")
+}
+
+
+//Exemple2
+
+protocol ChannelsTV {
+    var channel : String { get set }
+}
+
+struct MyChannels : ChannelsTV {
+    var channel: String
+}
+
+let abc = MyChannels(channel: "ABC Networks.")
+print(abc.channel)
+
+/// No is possible creat a protocol only with  set.
+
+
