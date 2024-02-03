c80efa0e6485a1d33db214d0b408b687f3f9ae84
Rafael Gonzaga via Xcode <rluisp@icloud.com>
2024-02-03 16:34:12 -0300

    Variables and constants, Types of Data, Operators, String interpolation

--- Project100DaysSwift.playground/Pages/DayThirteen.xcplaygroundpage/Contents.swift
+++ Project100DaysSwift.playground/Pages/DayThirteen.xcplaygroundpage/Contents.swift
@@ -1,7 +1,76 @@
-//: [Previous](@previous)
+//Consolidation 1
 
-import Foundation
+//DayThirteen - Variables and constants, Types of Data, Operators, String interpolation, arrays, Dicitionares, Conditional statements., Loops, Switch case
 
-var greeting = "Hello, playground"
 
-//: [Next](@next)
+//MARK: Variables and constants
+
+var name = "Tim McGraw"
+name = "Romeo"
+
+let name2 = "Tim McGraw"
+ 
+
+
+//MARK: Types of Data, String, Int, Double, Float...
+
+var name3: String
+name3 = "Tim McGraw"
+
+var age: Int
+age = 25
+
+var latitude: Double
+latitude = 36.166667
+
+var longitude: Float
+longitude = -86.783333
+
+//MARK: Operators
+///Math basic
+var a = 10
+a = a + 1
+a = a - 1
+a = a * a
+///Add then assign to
+var b = 10
+b += 10
+b -= 10
+
+///Some of these operators apply to other data types
+var a2 = 1.1
+var b2 = 2.2
+var c = a2 + b2
+
+var name1 = "Tim McGraw"
+var name4 = "Romeo"
+var both = name1 + " and " + name4
+
+///Operators  comparations
+c > 3
+c >= 3
+c > 4
+c < 4
+
+var name5 = "Tim McGraw"
+name5 == "Tim McGraw"
+
+
+var stayOutTooLate = true
+stayOutTooLate
+!stayOutTooLate
+
+
+var name6 = "Tim McGraw"
+name6 == "Tim McGraw"
+name6 != "Tim McGraw"
+
+
+//MARK: String Interpolation
+ 
+var name9 = "Tim McGraw"
+var age2 = 25
+var latitude1 = 36.166667
+
+"Your name is \(name9), your age is \(age2), and your latitude is \(latitude1)"
+
