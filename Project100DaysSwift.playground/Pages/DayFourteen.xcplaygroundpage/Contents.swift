04c9d29f831d523a245a6675ad3c42af8b72e3df
Rafael Gonzaga via Xcode <rluisp@icloud.com>
2024-02-04 10:22:17 -0300

    Funcitons

--- Project100DaysSwift.playground/Pages/DayFourteen.xcplaygroundpage/Contents.swift
+++ Project100DaysSwift.playground/Pages/DayFourteen.xcplaygroundpage/Contents.swift
@@ -1,7 +1,62 @@
-//: [Previous](@previous)
+//DayFourteen - Functions, Optionals, Optional chaining, Enumerations, Structs, Classes.
 
-import Foundation
+//MARK: - Funcitons
 
-var greeting = "Hello, playground"
+func printAlbumRelease(name: String, year: Int) {
+    print("\(name) was released in \(year)")
+}
 
-//: [Next](@next)
+printAlbumRelease(name: "Fearless", year: 2008)
+printAlbumRelease(name: "Speak Now", year: 2010)
+printAlbumRelease(name: "Red", year: 20)
+
+///External and internal parameter names
+func countLettersInString(string: String) {
+    print("The string \(string) has \(string.count) letters.")
+}
+
+countLettersInString(string: "Hello")
+
+///Specify an underscore, _, as the external parameter name.
+func countLettersInString(_ str: String) {
+    print("The string \(str) has \(str.count) letters.")
+}
+
+countLettersInString("HelloWorld")
+
+///Call the function with the parameter name “in”.
+func countLetters(in string: String) {
+    print("The string \(string) has \(string.count) letters.")
+}
+
+countLetters(in: "Swift")
+                
+///Return values   and switch/case.
+
+func albumIsTaylor(name: String) -> Bool {
+    if name == "Taylor Swift" { return true }
+    if name == "Fearless" { return true }
+    if name == "Speak Now" { return true }
+    if name == "Red" { return true }
+    if name == "1989" { return true }
+
+    return false
+}
+
+if albumIsTaylor(name: "Red") {
+    print("That's one of hers!")
+} else {
+    print("Who made that?!")
+}
+
+if albumIsTaylor(name: "Blue") {
+    print("That's one of hers!")
+} else {
+    print("Who made that?!")
+}
+                  
+
+func getMeaningOfLife() -> Int {
+    42
+}
+
