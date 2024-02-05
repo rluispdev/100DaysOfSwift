341a51062c4005fc907300465e5c5b40911f170b
Rafael Gonzaga via Xcode <rluisp@icloud.com>
2024-02-05 15:12:33 -0300

    Properties

--- Project100DaysSwift.playground/Pages/DayFifteen.xcplaygroundpage/Contents.swift
+++ Project100DaysSwift.playground/Pages/DayFifteen.xcplaygroundpage/Contents.swift
@@ -1,7 +1,55 @@
-//: [Previous](@previous)
+//DayFifteen - Properties, Static properties and methods, acess control, Polymorphism and typecasting, Closures
 
-import Foundation
+//MARK: Properties
 
-var greeting = "Hello, playground"
+struct Person {
+    var clothes: String
+    var shoes: String
 
-//: [Next](@next)
+    func describe() {
+        print("I like wearing \(clothes) with \(shoes)")
+    }
+}
+
+let taylor = Person(clothes: "T-shirts", shoes: "sneakers")
+let other = Person(clothes: "short skirts", shoes: "high heels")
+taylor.describe()
+other.describe()
+
+///Property observers
+struct Person1 {
+    var clothes: String {
+        willSet {
+            updateUI(msg: "I'm changing from \(clothes) to \(newValue)")
+        }
+
+        didSet {
+            updateUI(msg: "I just changed from \(oldValue) to \(clothes)")
+        }
+    }
+}
+
+func updateUI(msg: String) {
+    print(msg)
+}
+
+var taylor1 = Person1(clothes: "T-shirts")
+taylor1.clothes = "short skirts"
+
+
+///Computed properties
+
+struct Person2 {
+    var age: Int
+
+    var ageInDogYears: Int {
+        get {
+            return age * 7
+        }
+    }
+}
+
+var fan = Person2(age: 25)
+print(fan.ageInDogYears)
+
+
