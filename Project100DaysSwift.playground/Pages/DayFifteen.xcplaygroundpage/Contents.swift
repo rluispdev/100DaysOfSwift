c24870f6e18bf1ee6ebd8e896e2425134556acc9
Rafael Gonzaga via Xcode <rluisp@icloud.com>
2024-02-05 15:23:45 -0300

    Polymorphism and typecasting

--- Project100DaysSwift.playground/Pages/DayFifteen.xcplaygroundpage/Contents.swift
+++ Project100DaysSwift.playground/Pages/DayFifteen.xcplaygroundpage/Contents.swift
@@ -78,4 +78,81 @@ class TaylorFan1 {
  Private: this is the most restrictive option, and means the property is available only inside methods that belong to the type, or its extensions.
  */
 
-//MARK: -
+//MARK: - Polymorphism and typecasting
+
+class Album {
+    var name: String
+
+    init(name: String) {
+        self.name = name
+    }
+
+    func getPerformance() -> String {
+        return "The album \(name) sold lots"
+    }
+}
+
+class StudioAlbum: Album {
+    var studio: String
+
+    init(name: String, studio: String) {
+        self.studio = studio
+        super.init(name: name)
+    }
+
+    override func getPerformance() -> String {
+        return "The studio album \(name) sold lots"
+    }
+}
+
+class LiveAlbum: Album {
+    var location: String
+
+    init(name: String, location: String) {
+        self.location = location
+        super.init(name: name)
+    }
+
+    override func getPerformance() -> String {
+        return "The live album \(name) sold lots"
+    }
+}
+var taylorSwift = StudioAlbum(name: "Taylor Swift", studio: "The Castles Studios")
+var fearless = StudioAlbum(name: "Speak Now", studio: "Aimeeland Studio")
+var iTunesLive = LiveAlbum(name: "iTunes Live from SoHo", location: "New York")
+
+var allAlbums: [Album] = [taylorSwift, fearless, iTunesLive]
+
+for album in allAlbums {
+    print(album.getPerformance())
+}
+
+///Converting types with typecasting
+for album in allAlbums {
+    print(album.getPerformance())
+
+    if let studioAlbum = album as? StudioAlbum {
+        print(studioAlbum.studio)
+    } else if let liveAlbum = album as? LiveAlbum {
+        print(liveAlbum.location)
+    }
+}
+
+var taylorSwift1 = StudioAlbum(name: "Taylor Swift", studio: "The Castles Studios")
+var fearless1 = StudioAlbum(name: "Speak Now", studio: "Aimeeland Studio")
+
+var allAlbums1: [Album] = [taylorSwift, fearless]
+
+for album in allAlbums1 {
+    let studioAlbum1 = album as! StudioAlbum
+    print(studioAlbum1.studio)
+}
+
+
+///Converting common types with initializers
+//let number = 5
+//let text = number as! String
+let number = 5
+let text = String(number)
+print(text)
+
